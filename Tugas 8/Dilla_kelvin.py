8391c26ac2d9c95a1cd5c558e03d8c355e21c020
fadillahdf <148836112+fadillahdf@users.noreply.github.com>
2023-12-20 18:14:37 +0700

    Add files via upload

---
+++ Tugas 8/Dilla_kelvin.py
@@ -0,0 +1,66 @@
+from tkinter import Frame,Label,Entry,Button,YES,BOTH,END,Tk,W
+
+class FrmSuhu:
+    def __init__(self, parent, title):
+        self.parent = parent       
+        #self.parent.geometry("400x400")
+        self.parent.title(title)
+        self.parent.protocol("WM_DELETE_WINDOW", self.onKeluar)
+        self.aturKomponen()
+        
+    def aturKomponen(self):
+        mainFrame = Frame(self.parent, bd=10)
+        mainFrame.pack(fill=BOTH, expand=YES)      
+        # pasang Label
+        Label(mainFrame, text='Fadillah Dian Firdaus_220511081').grid(row=0, column=1,
+            sticky=W, padx=5, pady=5)
+        Label(mainFrame, text='Kelvin:').grid(row=1, column=0,
+            sticky=W, padx=5, pady=5)
+        Label(mainFrame, text="Celcius:").grid(row=3, column=0,
+            sticky=W, padx=5, pady=5)
+        Label(mainFrame, text="Reamur:").grid(row=4,column=0,sticky=W,padx=5,pady=5)
+        Label(mainFrame,text="Fahrenheit:").grid(row=5,column=0,sticky=W,padx=5,pady=5)
+
+        # pasang textbox
+        self.txtKelvin = Entry(mainFrame) 
+        self.txtKelvin.grid(row=1, column=1, padx=5, pady=5)  
+        self.txtCelcius = Entry(mainFrame) 
+        self.txtCelcius.grid(row=3, column=1, padx=5, pady=5)
+        self.txtReamur = Entry(mainFrame)
+        self.txtReamur.grid(row=4,column=1,padx=5,pady=5)
+        self.txtFahrenheit = Entry(mainFrame)
+        self.txtFahrenheit.grid(row=5,column=1,padx=5,pady=5)
+        # Pasang Button
+        self.btnHitung = Button(mainFrame, text='Hitung',
+            command=self.onHitung)
+        self.btnHitung.grid(row=2, column=1, padx=5, pady=5)
+        
+           
+    # fungsi "onHitung" berfungsi untuk menghitung konversi suhu 
+    def onHitung(self):
+        #konvesi celcius
+        K = int(self.txtKelvin.get())
+        value = (K - 273)
+        self.txtCelcius.delete(0,END)
+        self.txtCelcius.insert(END,str(value))
+
+        #konversi reamur
+        K = int(self.txtKelvin.get())
+        value = (4/5 * (K - 273))
+        self.txtReamur.delete(0,END)
+        self.txtReamur.insert(END,(str(value)))
+
+        #konversi fahrenheit
+        K = int(self.txtKelvin.get())
+        value = (9/5 * (K - 273)+32)
+        self.txtFahrenheit.delete(0,END)
+        self.txtFahrenheit.insert(END,(str(value)))
+
+    def onKeluar(self, event=None):
+        # memberikan perintah menutup aplikasi
+        self.parent.destroy()
+
+if __name__ == '__main__':
+    root = Tk()  
+    aplikasi = FrmSuhu(root, "Program Konversi Suhu")
+    root.mainloop() 
